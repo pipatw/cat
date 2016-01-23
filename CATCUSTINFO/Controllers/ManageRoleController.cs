f9278d8e53104026589b12582015f47dfd18d4e1
pipatw <mrpipatw@hotmail.com>
2016-01-24 00:46:41 +0700

    done

---
+++ CATCUSTINFO/Controllers/ManageRoleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CATCUSTINFO.Controllers
+{
+    public class ManageRoleController : Controller
+    {
+        // GET: ManageRole
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
